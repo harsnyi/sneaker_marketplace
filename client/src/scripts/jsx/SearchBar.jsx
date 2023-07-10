210d87f4cccffcba7b5cc90a05353cb5a65a4ea7
bener <bene.richard24@gmail.com>
2023-07-10 09:44:15 +0200

    search bar v1

--- client/src/scripts/jsx/SearchBar.jsx
+++ client/src/scripts/jsx/SearchBar.jsx
@@ -1,9 +1,22 @@
 import '../../assets/css/searchbar.css';
+import searchIcon from '../../assets/images/logo&icon/magnifying-glass-solid.svg';
 
 const SearchBar = () => {
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log('searching...');
+  };
+
   return (
     <>
-      <header className="search-bar"></header>
+      <header className="search-bar">
+        <form onSubmit={handleSearchSubmit}>
+          <input type="search" name="search" className="search-input" placeholder="Keresés . . ." autoComplete="off" required />
+          <button type="submit">
+            <img src={searchIcon} alt="" />
+          </button>
+        </form>
+      </header>
     </>
   );
 };

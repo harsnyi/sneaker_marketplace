59dd4ced06b5c2b14f21b62a31aaab9a353304c0
benericsi <bene.richard24@gmail.com>
2023-07-24 21:36:23 +0200

    imagecomponent with static blurhash

--- client/src/scripts/jsx/home/Home.jsx
+++ client/src/scripts/jsx/home/Home.jsx
@@ -2,11 +2,13 @@ import "../../../assets/css/home_page.css";
 
 import backgroundImage from "../../../assets/images/jordan/jordan-extended.png";
 
+import ImageComponent from "../other/ImageComponent.jsx";
 
 const Home = () => {
     return <>
         <header className="home-header">
-            <img className="home-header-background" src={backgroundImage} alt="Háttérkép" loading="lazy" />
+            <ImageComponent src={backgroundImage} alt="Háttérkép" className="home-header-background" blurhash="L798JQ0JNa~WxuNGt7s:0J?Hxu9Z"/>
+
   
 
         </header>

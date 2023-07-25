2b57fe69cad0696f8c42ca80b5afeb9a5fcaa1b1
benericsi <bene.richard24@gmail.com>
2023-07-25 19:08:44 +0200

    initialozed home header

--- client/src/scripts/jsx/home/Home.jsx
+++ client/src/scripts/jsx/home/Home.jsx
@@ -3,15 +3,14 @@ import "../../../assets/css/home_page.css";
 import backgroundImage from "../../../assets/images/jordan/jordan-extended.png";
 
 import LazyImage from "../other/LazyImage.jsx";
+import HomeHeaderContent from "./HomeHeaderContent";
 
 const Home = () => {
     return <>
-        <header className="home-header">
+        <section className="home-header">
             <LazyImage src={backgroundImage} alt="Háttérkép" className="home-header-background" blurhash="L798JQ0JNa~WxuNGt7s:0J?Hxu9Z"/>
-
-  
-
-        </header>
+            <HomeHeaderContent/>
+        </section>
     </>
  };
 export default Home;

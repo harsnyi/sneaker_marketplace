b54b1c1615541f51ee93eec6e153d2bd348545ad
harsnyi-main <harsanyitanulo@gmail.com>
2023-07-28 15:54:00 +0200

    Login,registration useStates

--- client/src/scripts/jsx/auth/LoginForm.jsx
+++ client/src/scripts/jsx/auth/LoginForm.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    // Access the 'email' and 'password' state variables to handle the login process
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // You can implement your login logic here, e.g., send a login request to the server
+  };
+
   return (
-    <form>
-      <label>E-Mail</label>
+    <form onSubmit={handleSubmit}>
+      <label>Email</label>
       <br />
-      <input type="email" />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <br />
       <label>Jelszó</label>
       <br />
-      <input type="password" />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <br />
       <button type="submit">Bejelentkezés</button>
+      
+    
     </form>
+
+    
   );
 };
 

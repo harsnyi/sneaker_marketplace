4edcf3e10dd7bc71c2c5b4f51e06b9c6bf288dc8
harsnyi-main <harsanyitanulo@gmail.com>
2023-08-02 19:32:22 +0200

    Small changes

--- server/Laced_project/user/urls.py
+++ server/Laced_project/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from user.views import HelloView
+from user.views import HelloView, register_view, login_view, secured_endpoint
 
 urlpatterns = [
     path('helloWorld', HelloView.as_view()),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('secured/', secured_endpoint, name='secured_endpoint'),
 ]

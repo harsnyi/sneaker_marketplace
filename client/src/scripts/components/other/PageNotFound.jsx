4074cb2f551fdd2761fceac732f0d8393c594dce
benericsi <bene.richard24@gmail.com>
2023-09-09 10:21:19 +0200

    redirect from pagenotfound after interval

--- client/src/scripts/components/other/PageNotFound.jsx
+++ client/src/scripts/components/other/PageNotFound.jsx
@@ -1,15 +1,33 @@
 import '../../../assets/css/not_found.css';
 
 import {NavLink} from 'react-router-dom';
+import {useEffect, useState} from 'react';
 
 const PageNotFound = () => {
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (secondsLeft > 1) {
+        setSecondsLeft(secondsLeft - 1);
+      } else {
+        clearInterval(interval);
+        // Automatically click the button inside the NavLink when the interval ends
+        document.getElementById('navLinkButton').click();
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [secondsLeft]);
+
   return (
     <div className="not-found-wrapper">
       <h1>
         <span>404 ...</span> <br /> A keresett oldal nem található!
       </h1>
       <h2>Hibás URL cím.</h2>
-      <NavLink to="/">
+      <p>Automatikus átírányítás {secondsLeft} másodpercen belül.</p>
+      <NavLink to="/" id="navLinkButton">
         <button className="btn-main btn-white">Vissza a főoldalra</button>
       </NavLink>
     </div>

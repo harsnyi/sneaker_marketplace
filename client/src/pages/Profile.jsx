7e65e72df9b16a3582b63771b0ea91d59a0fad02
harsnyi <harsanyitanulo@gmail.com>
2023-09-21 18:09:44 +0200

    listing the users for testing(function)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,51 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import useRefreshToken from '../hooks/useRefreshToken';
+import axios from '../setup/Axios';
 
 function Profile() {
+  const [users,setUsers] = useState([])
   const refresh = useRefreshToken();
   useEffect(() => {
     document.title = 'Profil | Footwr.';
+
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axios.get('/users',{
+          signal:controller.signal
+        });
+        console.log(response.data);
+        isMounted && setUsers(response.data);
+      }catch(error) {
+         console.error(error)
+      }
+    }
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    }
+
+
   }, []);
 
   return (
     <>
-      <h1>Hello from the profile page</h1>
+      <div>
+        <h2>Users list:</h2>
+
+        {users?.length
+        ? (
+          <ul>
+            {users.map((user,i) => <li key={i}>{user?.username}</li>)}
+          </ul>
+        ) : <p>No users to display</p>
+        }
+      </div>
       <button onClick={() => refresh()}>refresh</button>
     </>
   );
--- server/sneaker_api/user/views.py
+++ server/sneaker_api/user/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
 from .user_serializer import MyTokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from user.user_serializer import UserRegistrationSerializer
 from rest_framework import status
 from rest_framework.response import Response

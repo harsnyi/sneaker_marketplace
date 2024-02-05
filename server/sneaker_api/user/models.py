f6748d8c94523fb42f107988c891988432dc1070
Zsolt Harsányi <harsanyitanulo@gmail.com>
2024-02-05 20:09:01 +0100

    .gitignore, validation, little fixes

--- server/sneaker_api/user/models.py
+++ server/sneaker_api/user/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import re
+from django.core.exceptions import ValidationError
 
-#Nullable for development phase only
+
+def validate_email(email): 
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if not re.match(email_regex, email):
+        raise ValidationError(f"{email} nem helyes email cím.")
+
+
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True,null=True)
     location = models.CharField(max_length=30, blank=True,null=True)
     birth_date = models.DateField(blank=True,null=True)
-    email = models.EmailField(max_length=50,blank=True,null=True)
+    email = models.EmailField(validators=[validate_email],max_length=50,blank=True,null=True)
     phone_number = models.CharField(max_length=25,blank=True,null=True)
     gender = models.CharField(max_length=10,blank=True,null=True)
 
 def validate_allowed_roles(value):
     allowed_roles = [4001, 5002, 6003, 7004]
     if value not in allowed_roles:
-        raise ValidationError(f"{value} is not a valid role. Valid roles are {allowed_roles}.")
+        raise ValidationError(f"{value} nem egy valós jogosultság. A jogosultságok: {allowed_roles}.")
 
 class Role(models.Model):
     role = models.IntegerField(validators=[validate_allowed_roles],null=True, blank=True)

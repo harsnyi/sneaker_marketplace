de95bb6487e2d40cd8f16638d45d1a363aae2808
Zsolt Harsányi <harsanyitanulo@gmail.com>
2024-02-05 15:19:10 +0100

    JWT testing, JWT key

--- server/sneaker_api/user/urls.py
+++ server/sneaker_api/user/urls.py
@@ -4,6 +4,7 @@ from .views import(
     Update_access_token_view,
     Register_view,LogoutView,
     Authentication_view,
+    CheckAccessToken
 )
 
 urlpatterns = [
@@ -12,5 +13,5 @@ urlpatterns = [
     path('registration',Register_view.as_view(),name='registration'),
     path('token/logout',LogoutView.as_view(),name='logout'),
     path('token/authenticate',Authentication_view.as_view(),name='authenticate'),
-
+    path('token/check_access_token',CheckAccessToken.as_view(),name='check_access_token')
 ]
--- server/sneaker_api/user/views.py
+++ server/sneaker_api/user/views.py
@@ -4,12 +4,32 @@ from user.user_serializer import UserRegistrationSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework.views import APIView
 from .user_serializer import UserLoginSerializer
 from django.contrib.auth import authenticate
 from django.http import JsonResponse, HttpResponse
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 
+class CheckAccessToken(APIView):
+    def get(self, request):
+        authorization_header = request.headers.get('Authorization', '')
+
+        if not authorization_header.startswith('Bearer '):
+            return Response({"detail": "Invalid Authorization header format"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        access_token_string = authorization_header.split(' ')[-1]
+
+        try:
+            token = AccessToken(access_token_string)
+            token_payload = token.payload
+        except Exception as e:
+            return Response({"detail": "Invalid access token", "error": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if token_payload.get('exp') is not None:
+            return Response({"detail": "Access token is valid"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Access token is missing expiration time"}, status=status.HTTP_401_UNAUTHORIZED)
 
 #Registers a new user with the added serializer.
 #More logic needs to be done

60ee3a64b3cfdc666c5d3d94426c90daa1d53dbc
harsnyi <harsanyitanulo@gmail.com>
2024-02-22 17:28:37 +0100

    Fixed validators

--- server/sneaker_api/user/views.py
+++ server/sneaker_api/user/views.py
@@ -1,7 +1,5 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
